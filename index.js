2179d58ced9a2ee17045d3b675a1c90ad912f901
Westin Humble <westin.h@gmail.com>
2022-01-17 19:21:32 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+//let totalBatteries = 0;
+const totalBatteries = [4, 5, 3, 4, 4, 6, 5].reduce(function(accumulator, element){return element + accumulator});
+//totalBatteries += batteryBatches;
 
-// Code your solution here
+console.log(totalBatteries);
